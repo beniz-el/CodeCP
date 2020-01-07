5612c736e018ec238a57fc5a75a8e08f25472ac2
beniz-el <elbekrizineb97@gmail.com>
2020-01-07 11:03:34 +0100

    Ajout Message

---
+++ Message.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codecp;
+
+import java.util.Date;
+
+public class Message {
+    int Id_Msg;
+    String Contenu;
+    Date date;
+    boolean Lu;
+    
+    public void Envoyer_Msg(){
+    
+    }
+}
