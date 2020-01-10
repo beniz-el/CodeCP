b922f7a36e12960688d9f7c633cf4c6ee1cb34b6
beniz-el <59539157+beniz-el@users.noreply.github.com>
2020-01-10 22:28:02 +0100

    Update Message

--- Message.java
+++ Message.java
@@ -8,12 +8,66 @@ package codecp;
 import java.util.Date;
 
 public class Message {
-    int Id_Msg;
+    static int Id=0;
+    String Id_Msg;
+    String Id_User_emet;
+    String Id_User_recept;
     String Contenu;
     Date date;
     boolean Lu;
     
-    public void Envoyer_Msg(){
-    
+    public Message(String id1, String id2, String cont, boolean lu){
+         Integer i = ++Id;
+       Id_Msg = "M"+i.toString();
+       Id_User_emet = id1;
+       Id_User_recept = id2;
+       Contenu = cont;
+       Lu = lu;
     }
+
+    public void setIdUseremet(String Id_User_emet) {
+        this.Id_User_emet = Id_User_emet;
+    }
+
+    public void setIdUserrecept(String Id_User_recept) {
+        this.Id_User_recept = Id_User_recept;
+    }
+
+    public void setContenu(String Contenu) {
+        this.Contenu = Contenu;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setLu(boolean Lu) {
+        this.Lu = Lu;
+    }
+   
+    public String getIdMsg() {
+        return Id_Msg;
+    }
+
+    public String getIdUseremet() {
+        return Id_User_emet;
+    }
+
+    public String getIdUserrecept() {
+        return Id_User_recept;
+    }
+
+    public String getContenu() {
+        return Contenu;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public boolean getLu() {
+        return Lu;
+    }
+    
+    
 }
