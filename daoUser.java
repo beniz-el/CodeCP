1f0451892c0e9a23a59821a537ec1e1b3c604d10
beniz-el <59539157+beniz-el@users.noreply.github.com>
2020-01-10 10:35:35 +0100

    Update daoUser

--- daoUser.java
+++ daoUser.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -16,98 +17,122 @@ import java.util.logging.Logger;
 
 public class daoUser extends dao<User> {
     public daoUser(){
-        db = new Database();
+        Database db = new Database();
     }
     
-    public void create(User u){
+    public boolean create(User u){
+          boolean rs = false;
         try {
             PreparedStatement Pst;
-            Pst = db.con.prepareStatement( "INSERT INTO User VALUES(?,?)");
+            Pst = db.con.prepareStatement( "INSERT INTO User VALUES(?,?,?,?,?,?,?,?,?)");
             Pst.setInt(1, u.getIdUser());
             Pst.setString(2, u.getUsername());
             Pst.setString(3, u.getMdp());
-            if(Pst.executeUpdate()!=0){
-                System.out.println("Ajout effectuee");
+            Pst.setString(4, "");
+            Pst.setString(5, "");
+            Pst.setString(6, "");
+            Pst.setString(7, "");
+            Pst.setString(8, ""); 
+            Pst.setString(9, "");
+            
+            if(db.dmlQuery(Pst) == 1){
+                rs=true;
             }
-            else{
-                System.out.println("PB dans INSERT");
-            } 
+            
         } catch (SQLException ex) {
             Logger.getLogger(daoUser.class.getName()).log(Level.SEVERE, null, ex);
         }
+        return rs;
     }
     
     //ajouter ou modifier infos
-    public void update(User u){
+    public boolean update(User u, String Id){
+        boolean rs = false;
         try {
             PreparedStatement Pst;
-            Pst = db.con.prepareStatement("UPDATE User set Nom =?, Prenom =? ,Mdp=?, Tel=?, Email=? WHERE Id_User=?");
-            Pst.setString(1, u.getNom());
-            Pst.setString(2, u.getPrenom());
-            Pst.setString(3, u.getMdp());
-            Pst.setString(4, u.getTel());
-            Pst.setString(5, u.getEmail());
-            if(Pst.executeUpdate()!=0){
-                System.out.println("Mise a jour effectuee");
-            }
-            else{
-                System.out.println("user nexiste pas");
-            }
+            Pst = db.con.prepareStatement("UPDATE User set Username = ?, Mdp = ?, Nom =?, Prenom =? , Langage=?, Tel=?, E_mail=? , Photo = ? WHERE Id_User=?");
+            Pst.setString(1, u.getUsername());
+            Pst.setString(2, u.getMdp());
+            Pst.setString(3, u.getNom());
+            Pst.setString(4, u.getPrenom());
+            Pst.setString(5, u.getLangage());
+            Pst.setString(6, u.getTel());
+            Pst.setString(7, u.getEmail());
+            Pst.setString(8, u.getPhoto());
+            Pst.setInt(9, Integer.parseInt(Id));
+            
+           if(db.dmlQuery(Pst) == 1){
+               rs=true;
+           }
         } catch (SQLException ex) {
             Logger.getLogger(daoUser.class.getName()).log(Level.SEVERE, null, ex);
         }
    
-       
+       return rs;
                            
     }
     
     //supprimer
-    public void delete(User u){
+    public boolean delete(String id){
+        boolean rs = false;
         String sql = "DELETE FROM User WHERE Id_User=?";
         try {
             PreparedStatement stmt = db.con.prepareStatement(sql);
-            stmt.setInt(1, u.getIdUser());
+            stmt.setInt(1, Integer.parseInt(id));
             stmt.executeUpdate();
-            System.out.println("supprimee");
+            rs = true;
         } catch (SQLException ex) {
             Logger.getLogger(daoUser.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-        
+        return rs;
     }
     
     //lister
-    public Vector<User> all(){
-        
-            Vector<User> users = new Vector<User>();
-            
-            String findAllQuery = "SELECT *FROM User;";
-            ResultSet rs = db.query(findAllQuery);
-            try {
-                
-            while(rs.next()){
-                User u = new User();
-                
-                u.setUsername(rs.getString("Username"));
-                u.setNom(rs.getString("Nom"));
-                u.setPrenom(rs.getString("Prenom"));
-                u.setEmail(rs.getString("Email"));
-                u.setLangage(rs.getString("Langage"));
-                u.setTel(rs.getString("Tel"));
-                
-                users.add(u);
-                
+    public ResultSet all(){
+           ResultSet Rs =null ;
+        String req;
+            req = "select * from User";
+            Statement St;
+             try {
+                 St=db.con.createStatement();
+                 Rs= St.executeQuery(req);
+            while(Rs.next()){
+                System.out.println(Rs.getString(1)+"---->"+Rs.getString("Username")+"---->"+Rs.getString("Mdp")+"---->"+Rs.getString("Nom")+"---->"
+                        +Rs.getString("Prenom")+"---->"+Rs.getString("Langage")+"---->"+Rs.getString("E_mail")+"---->"+Rs.getString("Tel"));
             }
-            
-            } catch (SQLException ex) {
-            Logger.getLogger(daoUser.class.getName()).log(Level.SEVERE, null, ex);
+        
+        } catch (SQLException ex) {
+            System.out.println("PB dans la requete select");
         }
-            return users;
+      return Rs;
+
           
     }
-
+     
+    //find
     @Override
-    public User find(String id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public ResultSet find(String id) {
+        ResultSet Rs = null;
+        String sql = "Select * FROM User WHERE Id_User="+ id;
+        Statement St;
+             try {
+                 St=db.con.createStatement();
+                 Rs= St.executeQuery(sql);
+            while(Rs.next()){
+                System.out.println(Rs.getString(1)+"---->"+Rs.getString("Username")+"---->"+Rs.getString("Mdp")+"---->"+Rs.getString("Nom")+"---->"
+                        +Rs.getString("Prenom")+"---->"+Rs.getString("Langage")+"---->"+Rs.getString("E_mail")+"---->"+Rs.getString("Tel"));
+            }
+        
+        } catch (SQLException ex) {
+            System.out.println("PB dans la requete select");
+        }
+      return Rs;
+
     }
+
+
+
+    
+
 }
