4472bcce82bf3c676aa4f96c45570a999f841117
amal-krm <39573127+amal-krm@users.noreply.github.com>
2020-01-11 00:31:36 +0100

    Update CodeCP.java

--- CodeCP.java
+++ CodeCP.java
@@ -21,10 +21,36 @@ public class CodeCP {
     public static void main(String[] args) {
         // TODO code application logic here
         Database db = new Database();
-        DaoPrblms daoP = new DaoPrblms(db);
-//        probleme Pb = new probleme("ez21", "2", "edcvfr", "tgb", null);
-//        daoP.ModifierProbleme(Pb, "33");
-        daoP.SupprimerProbleme("212123");
+        //DaoCompetition daoC = new DaoCompetition(db);
+        Competition C = new Competition( "easy", true , null , null);
+        Competition C1 = new Competition( "easy", true , null , null);
+        Competition C2 = new Competition( "easy", true , null , null);
+        Competition C3 = new Competition( "hard", true , null , null);
+        probleme P = new probleme( null, null, null, null , null);
+        probleme P2 = new probleme( "2", "hard", "hard", "hard" , "hard");
+        DaoPrblms DaoP = new DaoPrblms(db);
+//        daoC.create(C1);
+//        daoC.create(C2);
+//        daoC.create(C3);
+//        daoC.delete("A12");
+//        daoC.update(C3, "AZE");
+//        daoC.AddProbleme("AZE", P);
+//        daoC.AddUser("amal", "AZE");
+//        daoC.AffcherListeProbleme("AZE");
+//        daoC.all();
+//        daoC.showUsers("AZE");
+//        DaoP.delete("1pp23");
+//        DaoP.update(P2, "123");
+        User U = new User("123", "1234", "@gmail");
+        User U2;
+        U2 = new User("12323", "1234", "amalalala@gmail" , "233","233","233","233","233");
+        daoUser daoU = new daoUser(db);
+        //daoU.create(U2);
+        //daoU.update(U2, "123");
+        //daoU.delete("123");
+//        daoU.all();
+        
+     
         
         
        
