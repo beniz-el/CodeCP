8ce1b050e767d24669a8dc1b6f930b719e37bd96
amal-krm <39573127+amal-krm@users.noreply.github.com>
2020-01-11 00:22:27 +0100

    Update DaoPrblms.java

--- DaoPrblms.java
+++ DaoPrblms.java
@@ -15,55 +15,35 @@ import javax.xml.transform.Result;
  *
  * @author Amal
  */
-public class DaoPrblms {
+public class DaoPrblms extends DAO<probleme>{
     Database db ;
     public DaoPrblms(Database db){
         this.db = db;
     }
-    public boolean AjouterProbleme(probleme obj){
-          PreparedStatement PS=null;
-        try {
-            PS = db.con.prepareStatement("INSERT INTO codecp.probleme VALUES (?, ?, ?, ?, ?);");
-            PS.setObject(1,obj.getIdProbleme()); 
-            PS.setObject(2,obj.getId_Solution()); 
-            PS.setObject(3,obj.getDescription()); 
-            PS.setObject(4,obj.getLevel()); 
-            PS.setObject(5,obj.getTitre()); 
-        } catch (SQLException ex) {
-            System.out.println("error create");
-        }
-        return (db.dmlQuery(PS) == 0) ? false : true;  
-        
-    }
-    public boolean ModifierProbleme(probleme obj , String id){
-        PreparedStatement PS=null;
-        try {
-            PS = db.con.prepareStatement("UPDATE codecp.probleme SET Id_Solution = ?, DescriptionPb = ?, Level = ?, Titre = ?  WHERE codecp.probleme.Id_Probleme= ?;");          
-            PS.setObject(1,obj.getId_Solution()); 
-            PS.setObject(2,obj.getDescription()); 
-            PS.setObject(3,obj.getLevel()); 
-            PS.setObject(4,obj.getTitre()); 
-            PS.setObject(5,id); 
-        } catch (SQLException ex) {
-            System.out.println("error update prblm");
-        }
-        return (db.dmlQuery(PS) == 0) ? false : true; 
-        
-    }
-    public boolean SupprimerProbleme(String id){
-        PreparedStatement PS=null;
-        try {
-            PS = db.con.prepareStatement("DELETE FROM codecp.probleme WHERE codecp.probleme.Id_Probleme = ?;");
-            PS.setObject(1,id); 
-        } catch (SQLException ex) {
-            System.out.println("error create");
-        }
-           
-        
-        return (db.dmlQuery(PS) == 0) ? false : true; 
-    }
-    public ResultSet AfficherProbleme(){
+  
+    
+    @Override
+    public ResultSet find(String id) {
         String req;
+            req = "select * from codecp.probleme where codecp.probleme.Id_Probleme='"+id+"';";
+            Statement St;
+            ResultSet Rs=null;
+             try {
+                 St=db.con.createStatement();
+                 Rs= St.executeQuery(req);
+            while(Rs.next()){
+                System.out.println(Rs.getString(1)+"---->"+Rs.getString(2)+"---->"+Rs.getString(3));
+            }
+        
+        } catch (SQLException ex) {
+            System.out.println("PB dans la requete select prblm");
+        }
+        return Rs;
+    }
+
+    @Override
+    public ResultSet all() {
+            String req;
             req = "select * from codecp.probleme";
             Statement St;
             ResultSet Rs=null;
@@ -78,6 +58,53 @@ public class DaoPrblms {
             System.out.println("PB dans la requete select prblm");
         }
         return Rs;
-    }        
+    }
+
+    @Override
+    public boolean create(probleme obj) {
+    PreparedStatement PS=null;
+        try {
+            PS = db.con.prepareStatement("INSERT INTO codecp.probleme VALUES (?, ?, ?, ?, ? , ?);");
+            PS.setObject(1,obj.getIdProbleme()); 
+            PS.setObject(2,obj.getId_Solution()); 
+            PS.setObject(3,obj.getDescription()); 
+            PS.setObject(4,obj.getLevel()); 
+            PS.setObject(5,obj.getTitre()); 
+            PS.setObject(6,obj.getTest()); 
+        } catch (SQLException ex) {
+            System.out.println("error create");
+        }
+        return (db.dmlQuery(PS) == 0) ? false : true;  
+    }
+
+    @Override
+    public boolean update(probleme obj, String id) {
+        PreparedStatement PS=null;
+        try {
+            PS = db.con.prepareStatement("UPDATE codecp.probleme SET Id_Solution = ?, DescriptionPb = ?, Level = ?, Titre = ?  WHERE codecp.probleme.Id_Probleme= ?;");          
+            PS.setObject(1,obj.getId_Solution()); 
+            PS.setObject(2,obj.getDescription()); 
+            PS.setObject(3,obj.getLevel()); 
+            PS.setObject(4,obj.getTitre()); 
+            PS.setObject(5,id); 
+        } catch (SQLException ex) {
+            System.out.println("error update prblm");
+        }
+        return (db.dmlQuery(PS) == 0) ? false : true; 
+    }
+
+    @Override
+    public boolean delete(String id) {
+        PreparedStatement PS=null;
+        try {
+            PS = db.con.prepareStatement("DELETE FROM codecp.probleme WHERE codecp.probleme.Id_Probleme = ?;");
+            PS.setObject(1,id); 
+        } catch (SQLException ex) {
+            System.out.println("error create");
+        }
+           
+        
+        return (db.dmlQuery(PS) == 0) ? false : true; 
+    }
     
 }
