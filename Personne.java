15a7e47969aa77cf70e0cf68ddd2efe62b68e1db
amal-krm <39573127+amal-krm@users.noreply.github.com>
2020-01-20 18:23:18 +0100

    Update Personne.java

--- Personne.java
+++ Personne.java
@@ -1,17 +1,31 @@
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package codecp;
+
 
 public class Personne {
-    static int id =0;
     String Username;
     
     public Personne(){
-               ++id;
-       Username = "U"+String.valueOf(id);
+       Integer id=0;
+        try {
+            id = daoPersonne.getCount();
+            //System.out.println("The id is " + id);
+        } catch (SQLException ex) {
+            Logger.getLogger(Personne.class.getName()).log(Level.SEVERE, null, ex);
+        }
+       
+       Username = "U"+(id++).toString();
+        System.out.println(Username);
     }
     
     public Personne(String user){
