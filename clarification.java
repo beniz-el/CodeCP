2027e728bd4dea9597154ab16e6706e315c6f86e
beniz-el <59539157+beniz-el@users.noreply.github.com>
2020-01-20 22:39:07 +0100

    Create clarification.java

---
+++ clarification.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codecp;
+
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class clarification extends Message{
+    String Id_Msg;
+    String Username;
+    String Adm_Username;
+    String Id_Probleme;
+    
+    public clarification(String User, String adm, String cont, String obj,String Id_prob){
+        super(cont,obj);
+        Integer id=0;
+        try {
+            id = daoClarification.getCount();
+        } catch (SQLException ex) {
+            Logger.getLogger(clarification.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Id_Msg = "Cl"+(id++).toString();
+        Username = User;
+        Adm_Username = adm;
+        Id_Probleme = Id_prob;
+    }
+
+    public void setId_Probleme(String Id_Probleme) {
+        this.Id_Probleme = Id_Probleme;
+    }
+
+    public String getId_Probleme() {
+        return Id_Probleme;
+    }
+
+     public String getId_Msg() {
+        return Id_Msg;
+    }
+    public String getUsername() {
+        return Username;
+    }
+
+    public String getAdm_Username() {
+        return Adm_Username;
+    }
+
+    public void setUsername(String Username) {
+        this.Username = Username;
+    }
+
+    public void setAdm_Username(String Adm_Username) {
+        this.Adm_Username = Adm_Username;
+    }
+    
+    
+}
