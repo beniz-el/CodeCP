576f7685b5a601669a0018561466220429055157
beniz-el <59539157+beniz-el@users.noreply.github.com>
2020-01-21 02:22:12 +0100

    Update Probleme.java

--- Probleme.java
+++ Probleme.java
@@ -5,39 +5,75 @@
  */
 package codecp;
 
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
 public class Probleme {
     String Id_Probleme;
-    String DescriptionPb;
-    String Level;
+    String Id_Solution;
+    String pathPb;
+    String Titre;
+    Level Level;
+    String pathTest;
     
-    public Probleme(){
-        
+    
+    public Probleme(String Id_sol, String pathProb, String titre, Level lev, String pathtest){
+        Integer id=0;
+        try {
+            id=daoProbleme.getCount();
+        } catch (SQLException ex) {
+            Logger.getLogger(Probleme.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        this.Id_Probleme="P"+id.toString();
+        this.Id_Solution= Id_sol;
+        this.Level = lev;
+        this.Titre=titre;
+        this.pathPb=pathProb;
+        this.pathTest=pathtest;
     }
-    public void AjouterProbleme(){
-        
-    }
-    public void ModifierProbleme(){
-        
-    }
-    public void SupprimerProbleme(){
-        
-    }
-    public void AfficherProbleme(){
-        
-    }
-    public String getIdProbleme(){
+
+    public String getId_Probleme() {
         return Id_Probleme;
     }
-    public String getDescription(){
-        return DescriptionPb;
+
+    public String getId_Solution() {
+        return Id_Solution;
     }
-    public void setDescription(String descrp){
-        DescriptionPb = descrp;
+
+    public String getPathPb() {
+        return pathPb;
     }
-    public String getLevel(){
+
+    public String getTitre() {
+        return Titre;
+    }
+
+    public Level getLevel() {
         return Level;
     }
-    public void setLevel(String lev){
-        Level = lev;
+
+    public String getPathTest() {
+        return pathTest;
     }
+
+    public void setId_Solution(String Id_Solution) {
+        this.Id_Solution = Id_Solution;
+    }
+
+    public void setPathPb(String pathPb) {
+        this.pathPb = pathPb;
+    }
+
+    public void setTitre(String Titre) {
+        this.Titre = Titre;
+    }
+
+    public void setLevel(Level Level) {
+        this.Level = Level;
+    }
+
+    public void setPathTest(String pathTest) {
+        this.pathTest = pathTest;
+    }
+    
 }
